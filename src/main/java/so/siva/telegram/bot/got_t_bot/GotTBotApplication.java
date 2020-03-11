f149f16eb02ac6796cf0671fba909d423b443f1b
A.Sivakon <got.telegram.bot@gmail.com>
2020-03-11 11:09:40 +0300

    test cron ping on heroku

--- src/main/java/so/siva/telegram/bot/got_t_bot/GotTBotApplication.java
+++ src/main/java/so/siva/telegram/bot/got_t_bot/GotTBotApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 import so.siva.telegram.bot.got_t_bot.config.InfoHouseCardsCommandsConfig;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties({InfoHouseCardsCommandsConfig.class})
 @PropertySource(value = "classpath:application-info_config.yml")
 public class GotTBotApplication {
@@ -17,5 +17,6 @@ public class GotTBotApplication {
 	public static void main(String[] args) {
         ApiContextInitializer.init();
 		SpringApplication.run(GotTBotApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Minsk"));
 	}
 }
